101734afbae4161e5b3c78648ef940509abff1b7
rodriaramayo90-hub <rodriaramayo90@gmail.com>
2026-07-13 13:22:51 +0200

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const playas = [
-  { nombre: "Samil", puntuacion: 87 },
-  { nombre: "Playa América", puntuacion: 84 },
-  { nombre: "Barra", puntuacion: 82 }
+    {
+        nombre: "Playa de la Magdalena",
+        municipio: "Cabanas",
+        lat: 43.426,
+        lon: -8.165
+    },
+    {
+        nombre: "Playa de Miño",
+        municipio: "Miño",
+        lat: 43.348,
+        lon: -8.207
+    },
+    {
+        nombre: "Playa de Perbes",
+        municipio: "Miño",
+        lat: 43.364,
+        lon: -8.186
+    },
+    {
+        nombre: "Playa de Sada",
+        municipio: "Sada",
+        lat: 43.356,
+        lon: -8.258
+    },
+    {
+        nombre: "Playa de Mera",
+        municipio: "Oleiros",
+        lat: 43.367,
+        lon: -8.380
+    },
+    {
+        nombre: "Playa de Sabón",
+        municipio: "Arteixo",
+        lat: 43.307,
+        lon: -8.511
+    }
 ];
 
-const tabla = document.getElementById("ranking");
-
-playas.forEach((playa, index) => {
-  tabla.innerHTML += `
-    <tr>
-      <td>${index + 1}</td>
-      <td>${playa.nombre}</td>
-      <td>${playa.puntuacion}</td>
-    </tr>
-  `;
-});
+console.log(playas);
