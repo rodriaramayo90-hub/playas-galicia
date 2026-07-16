5f198ac2238c57e111cfece720e1ef139769f889
rodriaramayo90-hub <rodriaramayo90@gmail.com>
2026-07-16 22:01:00 +0200

    cambio en como se obtiene la distancia
    ahora calcula distancia en coche en vez de distancia en linea recta

--- app.js
+++ app.js
@@ -128,25 +128,29 @@ const playas = [
   }
 ];
 
-function calcularDistancia(lat1, lon1, lat2, lon2) {
+async function calcularDistanciaCoche(
+  lat1,
+  lon1,
+  lat2,
+  lon2
+) {
 
-  const R = 6371;
+  const url =
+    `https://router.project-osrm.org/route/v1/driving/` +
+    `${lon1},${lat1};${lon2},${lat2}` +
+    `?overview=false`;
 
-  const dLat = (lat2 - lat1) * Math.PI / 180;
-  const dLon = (lon2 - lon1) * Math.PI / 180;
+  const respuesta = await fetch(url);
+  const datos = await respuesta.json();
 
-  const a =
-    Math.sin(dLat / 2) ** 2 +
-    Math.cos(lat1 * Math.PI / 180) *
-    Math.cos(lat2 * Math.PI / 180) *
-    Math.sin(dLon / 2) ** 2;
+  if (
+    !datos.routes ||
+    datos.routes.length === 0
+  ) {
+    return null;
+  }
 
-  const c = 2 * Math.atan2(
-    Math.sqrt(a),
-    Math.sqrt(1 - a)
-  );
-
-  return R * c;
+  return datos.routes[0].distance / 1000;
 }
 
 
